87439562b504a1e44e2864e2d96e39478cd9f9b9
yeoldeknob <tmckenna1988@gmail.com>
2022-11-21 08:06:24 -0500

    Moved sections around, added an expandable 'about me' option. Added icons to the services headers for more stylistic purposes.

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,12 @@
 const sneakerImage = document.querySelector('#silverpoint-img');
 const paterrosImage = document.querySelector('#paterros-img');
 const salonImage = document.querySelector('#salon-img');
+const moreButton = document.querySelector('.more');
 
 sneakerImage.addEventListener('click', redirectSneakers);
 paterrosImage.addEventListener('click', redirectRestaurant);
 salonImage.addEventListener('click', redirectSalon);
+moreButton.addEventListener('click', expandInfo);
 
 function redirectSneakers() {
     window.open('../shoe-store/index.html', '_blank');
@@ -16,4 +18,16 @@ function redirectRestaurant() {
 
 function redirectSalon() {
     window.open('../salon/index.html', '_blank');
+}
+
+function expandInfo() {
+    let content = document.querySelector('.content');
+    let text = document.querySelector('.more-text');
+    if (content.style.maxHeight) {
+        text.innerHTML = 'More';
+        content.style.maxHeight = null;
+    } else {
+        text.innerHTML = 'Less';
+        content.style.maxHeight = content.scrollHeight + 'px';
+    }
 }
