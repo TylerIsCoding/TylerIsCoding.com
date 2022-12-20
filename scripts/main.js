d1f0e5532109787039ed810a8bca11855539366c
Tyler McKenna <tmckenna1988@gmail.com>
2022-12-19 21:38:12 -0500

    Added the virtual bartender app to my portfolio

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,33 @@
 const sneakerImage = document.querySelector('#silverpoint-img');
-const paterrosImage = document.querySelector('#paterros-img');
+const bartenderImage = document.querySelector('#bartender-img');
 const salonImage = document.querySelector('#salon-img');
 const moreButton = document.querySelector('.more');
 
 sneakerImage.addEventListener('click', redirectSneakers);
-paterrosImage.addEventListener('click', redirectRestaurant);
+bartenderImage.addEventListener('click', redirectBartender);
 salonImage.addEventListener('click', redirectSalon);
 moreButton.addEventListener('click', expandInfo);
 
 function redirectSneakers() {
-    window.open('../shoe-store/index.html', '_blank');
+  window.open('../shoe-store/index.html', '_blank');
 }
 
-function redirectRestaurant() {
-    window.open('../restaurant/index.html', '_blank');
+function redirectBartender() {
+  window.open('../bartender/index.html', '_blank');
 }
 
 function redirectSalon() {
-    window.open('../salon/index.html', '_blank');
+  window.open('../salon/index.html', '_blank');
 }
 
 function expandInfo() {
-    let content = document.querySelector('.content');
-    let text = document.querySelector('.more-text');
-    if (content.style.maxHeight) {
-        text.innerHTML = 'More';
-        content.style.maxHeight = null;
-    } else {
-        text.innerHTML = 'Less';
-        content.style.maxHeight = content.scrollHeight + 'px';
-    }
+  let content = document.querySelector('.content');
+  let text = document.querySelector('.more-text');
+  if (content.style.maxHeight) {
+    text.innerHTML = 'More';
+    content.style.maxHeight = null;
+  } else {
+    text.innerHTML = 'Less';
+    content.style.maxHeight = content.scrollHeight + 'px';
+  }
 }
