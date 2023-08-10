6efedc8bb12873d0c51065ed4e0133a7a0e943e7
Tyler McKenna <tmckenna1988@gmail.com>
2023-08-10 15:55:28 -0400

    Added projects to the projects section. Modified CSS breakpoints. Added Gifs for each project. Updated client-side JS scripts to include new projects.

--- js/main.js
+++ js/main.js
@@ -1,33 +1,45 @@
-const sneakerImage = document.querySelector('#silverpoint-img');
-const bartenderImage = document.querySelector('#bartender-img');
-const salonImage = document.querySelector('#salon-img');
-const moreButton = document.querySelector('.more');
+const sneakerImage = document.querySelector("#silverpoint-img");
+const bartenderImage = document.querySelector("#bartender-img");
+const salonImage = document.querySelector("#salon-img");
+const jessiImage = document.querySelector("#jessinaff-img");
+const questlogImage = document.querySelector("#questlog-img");
+const moreButton = document.querySelector(".more");
 
-sneakerImage.addEventListener('click', redirectSneakers);
-bartenderImage.addEventListener('click', redirectBartender);
-salonImage.addEventListener('click', redirectSalon);
-moreButton.addEventListener('click', expandInfo);
+sneakerImage.addEventListener("click", redirectSneakers);
+bartenderImage.addEventListener("click", redirectBartender);
+salonImage.addEventListener("click", redirectSalon);
+jessiImage.addEventListener("click", redirectJessi);
+questlogImage.addEventListener("click", redirectQuest);
+moreButton.addEventListener("click", expandInfo);
 
 function redirectSneakers() {
-  window.open('../shoe-store/index.html', '_blank');
+    window.open("../shoe-store/index.html", "_blank");
 }
 
 function redirectBartender() {
-  window.open('../restaurant/index.html', '_blank');
+    window.open("../restaurant/index.html", "_blank");
 }
 
 function redirectSalon() {
-  window.open('../salon/index.html', '_blank');
+    window.open("../salon/index.html", "_blank");
+}
+
+function redirectJessi() {
+    window.open("https://www.jessinaff.com", "_blank");
+}
+
+function redirectQuest() {
+    window.open("https://quest-log.cyclic.app/dashboard", "_blank");
 }
 
 function expandInfo() {
-  let content = document.querySelector('.content');
-  let text = document.querySelector('.more-text');
-  if (content.style.maxHeight) {
-    text.innerHTML = 'More';
-    content.style.maxHeight = null;
-  } else {
-    text.innerHTML = 'Less';
-    content.style.maxHeight = content.scrollHeight + 'px';
-  }
+    let content = document.querySelector(".content");
+    let text = document.querySelector(".more-text");
+    if (content.style.maxHeight) {
+        text.innerHTML = "More";
+        content.style.maxHeight = null;
+    } else {
+        text.innerHTML = "Less";
+        content.style.maxHeight = content.scrollHeight + "px";
+    }
 }
